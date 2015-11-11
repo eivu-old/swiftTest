776e7e5beaa43acd48cda823524fadc4ba84841d
Robert Jenkins <jinx@starbuck.home>
2015-11-11 14:15:13 -0500

    Initial Commit

---
+++ eivuTest3/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  eivuTest3
+//
+//  Created by Robert Jenkins on 11/11/15.
+//  Copyright © 2015 Robert Jenkins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
